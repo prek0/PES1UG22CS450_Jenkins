00b4caef3c1a7e1fa780c44608f33da4640cd188
Preksha <148479563+prek0@users.noreply.github.com>
2025-03-18 16:08:05 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(void) {
+  cout << "PES1UG22CS450 --- CC --- LAB-8\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
